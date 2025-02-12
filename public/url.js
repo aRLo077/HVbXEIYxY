14ca48d0efe68a3e1b13749eba6e3ddbfa78d26b
aRLo077 <hostels-timer0z@icloud.com>
2025-02-12 02:44:41 +0000

    优化了 网络请求 模块的性能

---
+++ public/url.js
@@ -0,0 +1,16 @@
+// Version 3 (First stable beta version)
+// textarea -> overflow hidden
+
+// url @overcoded
+// SERVER_URL = `wss://${window.location.hostname}/socket`
+// UPLOAD_URL = `https://${window.location.hostname}`
+
+//newline
+
+// @localhost
+
+// SERVER_URL = "ws://overcoded.tk:8500"
+// UPLOAD_URL = "http://overcoded.tk:8600"
+
+SERVER_URL = `ws://${window.location.hostname}:8500/`
+UPLOAD_URL = `http://${window.location.hostname}:8600`
